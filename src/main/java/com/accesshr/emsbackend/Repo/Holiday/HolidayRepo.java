f3fabdb981f17dbf9397ec73f18e6087553a9cf3
MTL1012 <raviteja49@gmail.com>
2025-04-04 15:32:22 +0530

    HolidayUpdat

--- src/main/java/com/accesshr/emsbackend/Repo/Holiday/HolidayRepo.java
+++ src/main/java/com/accesshr/emsbackend/Repo/Holiday/HolidayRepo.java
@@ -1,6 +1,10 @@
 package com.accesshr.emsbackend.Repo.Holiday;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.accesshr.emsbackend.Entity.Holiday;
 
@@ -10,5 +14,8 @@ public interface HolidayRepo extends JpaRepository<Holiday, Integer> {
 	void deleteAllById(int id);
 
 	Holiday getAllById(int id);
+	
+	@Query("DELETE FROM Holiday c WHERE c.date <= :currentDate")
+	void deletePreviousHoliday(@Param("currentDate") LocalDate currentDate);
 	
 }
